5c09ef705de56f85278e8c237680ae846cb3b50d
Matej Petric <matej.petric@digisence.agency>
2023-07-12 14:51:04 +0200

    fix

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,23 +1,46 @@
 import { Helmet } from 'react-helmet';
-import {
-  CssBaseline,
-  Grid,
-  Paper,
-  Box,
-  Typography,
-  TextField,
-  Button,
-  Link,
-  FormControlLabel,
-  Checkbox,
-  Avatar,
-} from '@mui/material';
+import { CssBaseline, Paper, Checkbox } from '@mui/material';
 import { BsFillLockFill } from 'react-icons/bs';
-import { useTheme } from '@mui/material/styles';
-import { styled as muiStyled } from '@mui/material/styles';
+import { styled } from '@mui/system';
+import BackgroundImage from './../../../assets/img/backgoundLogin.jpg';
+import { Grid, Typography, TextField, Button, Link, FormControlLabel, Avatar } from '@mui/material';
 
+const StyledGrid = styled(Grid)(({ theme }) => ({
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundImage: `url(${BackgroundImage})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+}));
 
-const ToggleButton = muiStyled('button')(({ theme }) => ({
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  marginTop: '5%',
+  color: theme.palette.secondary.contrastText,
+}));
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  color: theme.palette.secondary.contrastText,
+  margin: '2%'
+}));
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  width: '100%',
+  color: theme.palette.secondary.contrastText,
+  borderColor: theme.palette.secondary.textFieldBorder,
+}));
+const StyledFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
+  color: theme.palette.secondary.contrastText,
+}));
+const StyledButton = styled(Button)(({ theme }) => ({
+  marginTop: '3px',
+  marginBottom: '2px',
+  backgroundColor: theme.palette.secondary.contrastText,
+}));
+const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.secondary.contrastText,
+}));
+const ToggleButton = styled(Button)(({ theme }) => ({
   backgroundColor: 'transparent',
   border: 'solid',
   color: theme.palette.secondary.contrastText,
@@ -25,12 +48,25 @@ const ToggleButton = muiStyled('button')(({ theme }) => ({
   fontSize: 14,
   outline: 'none',
   margin: 20,
-  padding: 20
+  padding: 20,
+}));
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  maxWidth: '400px',
+  paddingLeft: '2%',
+  paddingRight: '2%',
+  backgroundColor: theme.palette.secondary.formBackground,
+  color: theme.palette.secondary.contrastText,
 }));
 
-const Login = ({ isDarkMode, toggleDarkMode }: any) => {
+interface paletteType {
+  isDarkMode: boolean,
+  toggleDarkMode: () => void;
+}
 
-  const theme = useTheme();
+const Login = ({ isDarkMode, toggleDarkMode }: paletteType) => {
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -51,108 +87,60 @@ const Login = ({ isDarkMode, toggleDarkMode }: any) => {
         />
       </Helmet>
       <CssBaseline />
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <Grid
-          sm={12}
-          md={12}
-          lg={12}
-          sx={{
-            backgroundImage:
-              'url(https://juzni.hr/media/k2/items/cache/24fc8d3f0c18a343756c9051aeb41a71_L.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Box
-            component={Paper}
-            elevation={6}
-            sx={{
-              p: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '400px',
-              backgroundColor: theme.palette.secondary.formBackground,
-              color: theme.palette.secondary.contrastText,
-            }}
-          >
-            <Avatar sx={{ m: 1, color: theme.palette.secondary.contrastText }}>
-              <BsFillLockFill />
-            </Avatar>
-            <Typography component="h1" variant="h5" sx={{ color: theme.palette.secondary.contrastText }}>
-              Prijava
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Adresa"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                InputLabelProps={{
-                  style: { color: theme.palette.secondary.contrastText },
-                }}
-                inputProps={{ style: { color: theme.palette.secondary.contrastText } }}
-                sx={{ borderColor: theme.palette.secondary.textFieldBorder }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Lozinka"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                InputLabelProps={{
-                  style: {
-                    color: theme.palette.secondary.contrastText,
-                  },
-                }}
-                inputProps={{ style: { color: theme.palette.secondary.contrastText } }}
-                sx={{ borderColor: theme.palette.secondary.textFieldBorder }}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" sx={{ color: theme.palette.secondary.contrastText }} />}
-                label="Zapamti moju prijavu"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, bgcolor: theme.palette.secondary.contrastText }}
-              >PRIJAVI SE
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Zaboravljena Lozinka
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Nemaš račun? Registriraj se!"}
-                  </Link>
-                </Grid>
+      <StyledGrid container as="main">
+        <StyledPaper elevation={6}>
+          <StyledAvatar>
+            <BsFillLockFill />
+          </StyledAvatar>
+          <StyledTypography as="h1" variant="h5">
+            Prijava
+          </StyledTypography>
+          <form noValidate onSubmit={handleSubmit}>
+            <StyledTextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Adresa"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <StyledTextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Lozinka"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <StyledFormControlLabel
+              control={<Checkbox value="remember" />}
+              label="Zapamti moju prijavu"
+            />
+            <StyledButton type="submit" fullWidth variant="contained">
+              PRIJAVI SE
+            </StyledButton>
+            <Grid container>
+              <Grid item xs>
+                <StyledLink href="#">
+                  Zaboravljena Lozinka
+                </StyledLink>
               </Grid>
-            </Box>
-            <ToggleButton onClick={toggleDarkMode} sx={{ color: theme.palette.secondary.contrastText }}>
-              {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-            </ToggleButton>
-          </Box>
-        </Grid>
-      </Grid>
+              <Grid item>
+                <StyledLink href="#">
+                  {"Nemaš račun? Registriraj se!"}
+                </StyledLink>
+              </Grid>
+            </Grid>
+          </form>
+          <ToggleButton onClick={toggleDarkMode}>
+            {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+          </ToggleButton>
+        </StyledPaper>
+      </StyledGrid>
     </>
   );
 };
--- src/app/routes/main.tsx
+++ src/app/routes/main.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Library } from '../pages/deleteThisLater/DeleteMeLater';
 import { Home } from '../pages/home/Home';
 import Login from '../pages/login/Login';
 
 interface MainRoutesProps {
-  isDarkMode: boolean;
-  toggleDarkMode: () => void;
+  isDarkMode: boolean
+  toggleDarkMode: () => void
 }
 
-export const MainRoutes: React.FC<MainRoutesProps> = ({ isDarkMode, toggleDarkMode }) => {
+export const MainRoutes = ({ isDarkMode, toggleDarkMode }: MainRoutesProps) => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
