c628401fa5200079136d1d89fcd0850edfd132fe
Zetchu <shmungula@gmail.com>
2023-09-19 14:00:18 +0200

    Sve zivo spojeno, dodano, probati custom card small za mobile?

--- src/pages/dashboard/one.tsx
+++ src/pages/dashboard/one.tsx
@@ -1,17 +1,20 @@
 import { Helmet } from 'react-helmet-async';
 // sections
 import OneView from 'src/sections/one/view';
+import { DesktopViewOne } from 'src/sections/one/view-desktop';
 
 // ----------------------------------------------------------------------
 
 export default function Page() {
+  const isDesktop = window.innerWidth > 1100; // Or whatever breakpoint you consider as "desktop"
+
   return (
     <>
       <Helmet>
         <title> Dashboard: One</title>
       </Helmet>
 
-      <OneView />
+      {isDesktop ? <DesktopViewOne /> : <OneView />}
     </>
   );
 }
