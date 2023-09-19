95bb5f5cb266dc1af59ff0696354c8d7ea80cf48
Matej Petrić <mat53c@Matejs-MBP.home>
2023-09-19 12:46:19 +0200

    dashboard mobile UI and API

--- src/sections/three/view.tsx
+++ src/sections/three/view.tsx
@@ -1,30 +1,81 @@
-// @mui
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-// components
 import { useSettingsContext } from 'src/components/settings';
+import { Container, Typography, Box, Grid, } from '@mui/material';
+import CustomCard from 'src/components/custom-card/custom-card';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { green, red } from '@mui/material/colors';
+import { useTheme } from '@mui/material/styles';
+import ScrollableContainer from 'src/components/scrollable-container/scrollable-container';
+import StatusCard from 'src/components/status-card/status-card';
+import SectionWrapper from 'src/components/section-wrapper/section-wrapper';
 
-// ----------------------------------------------------------------------
+
+const dummyData = [
+  {
+    imgUrl: "/assets/images/mandzukicPerisic.jpg",
+    cardText: "Card 1",
+    linkTo: "/dashboard/quiz",
+  },
+  {
+    imgUrl: "/assets/images/mandzukicPerisic.jpg",
+    cardText: "Card 2",
+    linkTo: "/dashboard/quiz",
+  },
+  {
+    imgUrl: "/assets/images/mandzukicPerisic.jpg",
+    cardText: "Card 2",
+    linkTo: "/dashboard/quiz",
+  },
+  {
+    imgUrl: "/assets/images/mandzukicPerisic.jpg",
+    cardText: "Card 2",
+    linkTo: "/dashboard/quiz",
+  },
+  {
+    imgUrl: "/assets/images/mandzukicPerisic.jpg",
+    cardText: "Card 2",
+    linkTo: "/dashboard/quiz",
+  }
+];
 
 export default function ThreeView() {
   const settings = useSettingsContext();
+  const theme = useTheme();
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
-      <Typography variant="h4"> Page Three </Typography>
+      <Typography variant="h2" color="error">Kvizovi</Typography>
+      <SectionWrapper title="Najnoviji">
 
-      <Box
-        sx={{
-          mt: 5,
-          width: 1,
-          height: 320,
-          borderRadius: 2,
-          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
-          border: (theme) => `dashed 1px ${theme.palette.divider}`,
-        }}
-      />
+        <ScrollableContainer>
+          {dummyData.map((data, index) => (
+            <Box key={index} sx={{ flex: '0 0 auto', width: '60%', maxWidth: '300px' }}>
+              <CustomCard imgUrl={data.imgUrl} cardText={data.cardText} linkTo={data.linkTo} />
+            </Box>
+          ))}
+        </ScrollableContainer>
+      </SectionWrapper>
+      <Box borderRadius={2} p={2} mt={2} sx={{
+        bgcolor: theme.palette.background.default,
+        [theme.breakpoints.up('md')]: {
+          bgcolor: theme.palette.background.neutral
+        },
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <StatusCard icon={<CheckIcon fontSize="large" sx={{ color: green[500], display: { xs: 'none', sm: 'inline' } }} />} number={21} text="Rjesen!" />
+        <StatusCard icon={<CloseIcon fontSize="large" sx={{ color: red[500], display: { xs: 'none', sm: 'inline' } }} />} number={25} text="Jos Cekaju!" />
+      </Box>
+      <SectionWrapper title="Svi kvizovi">
+        <Grid container spacing={2}>
+          {dummyData.map((data, index) => (
+            <Grid key={index} item xs={12} sm={6} md={4} lg={4}>
+              <CustomCard imgUrl={data.imgUrl} cardText={data.cardText} linkTo={data.linkTo} />
+            </Grid>
+          ))}
+        </Grid>
+      </SectionWrapper>
     </Container>
   );
 }
